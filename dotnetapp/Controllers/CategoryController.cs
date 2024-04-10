308d932e12d2c3e3a8ce6158626676fa74a9a3fc
Shubhika <Shubhikasingh25@gmail.com>
2024-04-10 17:09:28 +0530

    initial commit

---
+++ dotnetapp/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using dotnetapp.Data;
+using dotnetapp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetapp.Controllers
+{
+    public class CategoryController : Controller
+
+    {
+
+        private readonly ApplicationDbContext _db;
+        public  CategoryController(ApplicationDbContext db)
+        {
+            _db = db;   
+                
+        }
+        public IActionResult Index()
+        {
+
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+                return View();
+        }
+    }
+}
---
+++ dotnetapp/Migrations/20240408114822_addcategorydata.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcategorydata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
